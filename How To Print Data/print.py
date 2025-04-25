7598fc3c6f6594c76738a0a8663a8a6046a233af
Shabhaizafar <zafarshabhai03@gmail.com>
2025-04-25 09:01:18 +0530

    Python start

---
+++ How To Print Data/print.py
@@ -0,0 +1,51 @@
+# Multiline Comment : 
+'''
+comments in Python : 
+main comment : 
+    1. Single Line  :   # 
+    - multiline Comment 
+'''
+
+"""
+comments in Python : 
+main comment : 
+    1. Single Line  :   # 
+    - multiline Comment 
+"""
+
+
+#  How to Print Data in a Python : 
+# print("Hello Everyone!!")
+
+# print(123)
+
+# print(True)
+
+
+# 1. print() Function : 
+# default : 
+# sep=" "     white space
+    # But we can Change !!!
+# end="\n"    newline
+    # But we can Change !!!
+
+# Print Single Data in a Single Print() :
+# print("Hello Everyone")
+# Print Multiple Data in a Single Print() :
+# print("Hello Everyone","Welcome to Python World!!")
+
+
+# sep="" in a print : 
+# print("Hello Everyone","Welcome to Python World!!",sep="")
+# print("Hello Everyone","Welcome to Python World!!",sep="   ")
+# print("Hello Everyone","Welcome to Python World!!",sep=".")
+# print("Hello Everyone","Welcome to Python World!!","Python",sep="\n\n")
+
+
+# Print Multiple Data with Multiple Print() :
+# print("Hello Everyone",end=",")
+# print("Hello")
+
+
+
+
