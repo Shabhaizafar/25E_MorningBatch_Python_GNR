3db35aaa5eafa888f047bc8e6a0adc172a3d1062
Shabhaizafar <zafarshabhai03@gmail.com>
2025-04-30 09:07:07 +0530

    Operators,Datatype

---
+++ DataTypes/datatype1.py
@@ -0,0 +1,66 @@
+
+'''
+
+### **1. Numeric Types**
+- `int` > Integer (e.g., `10`, `-5`)
+- `float` > Floating point (e.g., `3.14`, `-0.01`)
+- `complex` > Complex number (e.g., `2 + 3j`)
+
+'''
+# num1 = 12
+# num2 = 1.0
+# num3 = 23+56j
+
+# print(num1)
+# print(type(num1))
+# print(num2)
+# print(type(num2))
+# print(num3)
+# print(type(num3))
+
+
+
+
+'''
+
+### **2. Sequence Types**
+- `str` > String (e.g., `'hello'`, `"world"`)
+- `list` > List (e.g., `[1, 2, 3]`)
+- `tuple` > Tuple (e.g., `(1, 2, 3)`)
+- `set` > Unordered, unique items (e.g., `{1, 2, 3}`)
+- `frozenset` > Immutable version of a set
+'''
+# mystr1 = "Zafar"
+# mystr2 = 'Royal'
+# mystr3 = """Hehehe"""
+# mystr4 = '''ByeBye'''
+
+# print(mystr1)
+# print(type(mystr1))
+# print(mystr2)
+# print(type(mystr2))
+# print(mystr3)
+# print(type(mystr3))
+# print(mystr4)
+# print(type(mystr4))
+
+
+
+'''
+
+---
+
+### **3. Dict Type**
+- `dict` > Dictionary (e.g., `{'a': 1, 'b': 2}`)
+
+---
+
+### **4. Boolean Type**
+- `bool` > Boolean (`True`, `False`)
+
+---
+
+
+### **5. None Type**
+- `NoneType` > Represents the absence of a value (`None`)
+'''
---
+++ Operators/operator1.py
@@ -0,0 +1,150 @@
+'''
+
+### **Arithmetic Operators**
+- `+`
+- `-`
+- `*`
+- `/`
+- `%`
+- `**`
+- `//`
+
+'''
+# num1 = 12
+# print("Num1 : ",num1)
+# num2 = 2
+# print("Num2 : ",num2)
+
+# print("Addition : ",num1+num2)
+# print("Subtraction : ",num1-num2)
+# print("Division : ",num1/num2)
+# print("Multiplication : ",num1*num2)
+# print("Modulo : ",num1%num2)
+
+# base = 2
+# power = 5
+# print("Power : ",base ** power)
+
+# num1 = 12
+# num2 = 5
+# print("Floor Division : ",num1//num2)
+'''
+
+### **Assignment Operators**
+- `=`
+- `+=`
+- `-=`
+- `*=`
+- `/=`
+- `%=`
+- `**=`
+- `//=`
+
+'''
+# num1  = 10
+# num2 = 2
+
+# num1**=num2  #  num1 = num1 ** num2  = 10 ** 2
+
+# num1//=num2 #   num1 = num1 // num2  = 10 // 2
+
+# print(num1)
+
+
+
+'''
+
+### **Comparison Operators**
+- `==`
+- `!=`
+- `>`
+- `<`
+- `>=`
+- `<=`
+
+'''
+# num1 = 10 
+# num2 = 2
+
+# print(num1==num2)
+# print(num1<num2)
+# print(num1>num2)
+# print(num1<=num2)
+# print(num1>=num2)
+# print(num1!=num2)
+
+
+'''
+
+### **Logical Operators**
+- `and`
+- `or`
+- `not`
+
+'''
+
+# num1 = 10 
+# num2 = 2
+
+# print(num1>num2 and num2>1)
+# print(num1>num2 and num2>9)
+
+
+# print(0 and num2)
+
+# print(num1>num2 or num2>9)
+# print(num1 or num2)
+
+# print(not num1>num2)
+
+'''
+
+### **Bitwise Operators**
+- `&`
+- `|`
+- `^`
+
+- `~`
+- `<<`
+- `>>`
+
+'''
+
+# num1 = 10  # 1010
+# num2 = 2   # 0010
+# ----------------
+# &          0010       = 2
+# |          1010       = 10
+# ^(X-OR)    1000       = 8
+
+
+
+# print(num1 & num2)
+# print(num1 | num2)
+# print(num1 ^ num2)
+
+
+#  (~number)   =  -(number+1)
+#   (~10)      =  -(10+1) = -11
+#   (~-6)      =  -(-6+1) = 5
+# print(~num1)
+
+# num1 = 10  # 1010
+# num2 = 2   
+
+# print(num1<<num2) #  101000 
+
+# print(num1>>num2)  #   0010
+'''
+
+### **Membership Operators**
+- `in`
+- `not in`
+
+---
+
+### **Identity Operators**
+- `is`
+- `is not`
+
+'''
