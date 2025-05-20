2f2a99b0575b1b88ac5cedb4efac79ee49e394aa
Shabhaizafar <zafarshabhai03@gmail.com>
2025-05-20 10:31:04 +0530

    String

---
+++ DataTypes/string.py
@@ -0,0 +1,65 @@
+# mystr = "Royal"
+# mystr = 'Royal2'
+
+# mystr = '''Royal3'''
+# mystr = """Royal4"""
+
+# mystr = '''Hello Everyone,
+# Welcome to Python.'''
+# print(mystr)
+
+# Python Comment : 
+# Single Line  
+# Multiple Line X
+
+# '''
+# This is not Ectual Multiline Comment
+# '''
+# """
+# This is not Ectual Multiline Comment
+# """
+
+
+mystr = "Royal"
+#index   01234
+# -vd    54321
+print(mystr)
+
+# len() : 
+# print(len(mystr))
+
+# print(mystr[2])
+
+# print(mystr[2:])
+
+# print(mystr[2:10])
+# print(mystr[10:2])   # not Valid
+# print(mystr[:10])
+
+# print(mystr[0:10:2])
+
+# print(mystr[0])
+# print(mystr[-5])  # 
+
+# print(mystr[-5 : -2])
+# print(mystr[-5 : 2])
+
+# print(mystr[-5 : 3 : -1])
+
+"""
+C : 
+
+# mystr.capitalize
+# mystr.casefold
+# mystr.center
+# mystr.count
+"""
+# print(mystr.capitalize())
+# print(mystr.casefold())
+# print(mystr.center(29,"-"))
+# print(mystr.count('o'))    # 3
+# print(mystr.count('oy'))   # 1
+# print(mystr.count('Z'))    # 0
+
+
+
